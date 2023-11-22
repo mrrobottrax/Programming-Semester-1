4fa31cd20ab97db5bed9139db10d8ce6ed533a99
Andrew <mrrobottrax@gmail.com>
2023-11-21 22:40:27 -0500

    Add enemy states

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,11 +12,10 @@ public class Enemy : MonoBehaviour
 	enum AiFlags
 	{
 		None = 0,
-		Scared = 1 << 0,
-		AwareOfPlayer = 1 << 1,
-		Tough = 1 << 2
+		Coward = 1 << 0,
+		Brave = 1 << 1
 	}
-	AiFlags flags;
+	[SerializeField] AiFlags flags;
 
 	enum AiState
 	{
@@ -26,6 +25,13 @@ public class Enemy : MonoBehaviour
 	}
 	AiState state;
 
+	bool canSeePlayer = false;
+	float health = 100;
+
+	Vector2 wanderDir = Vector2.zero;
+	float wanderTimer = 0;
+	const float wanderTime = 2;
+
 	private void Awake()
 	{
 		characterMove = GetComponent<CharacterMove>();
@@ -33,27 +39,82 @@ public class Enemy : MonoBehaviour
 
 	private void FixedUpdate()
 	{
-		SetFlags();
+		GetData();
+		GetState();
+		PickMoveDir();
+	}
 
-		if (flags.HasFlag(AiFlags.AwareOfPlayer))
+	void PickMoveDir()
+	{
+		switch (state)
 		{
-			Vector3 dir3 = player.position - transform.position;
-			Vector2 dir = new(dir3.x, dir3.z);
+			case AiState.Wandering:
+				{
+					wanderTimer -= Time.fixedDeltaTime;
+					if (wanderTimer <= 0)
+					{
+						wanderTimer = wanderTime;
 
-			characterMove.moveVector = dir.normalized;
-		}
-		else
-		{
-			characterMove.moveVector = Vector2.zero;
+						// random int to make movement a little less random
+						wanderDir = new Vector2(UnityEngine.Random.Range(-1, 2), UnityEngine.Random.Range(-1, 2)).normalized;
+					}
+
+					characterMove.moveVector = wanderDir;
+				}
+				break;
+			case AiState.Attacking:
+				{
+					Vector3 dir3 = player.position - transform.position;
+					Vector2 dir = new(dir3.x, dir3.z);
+
+					characterMove.moveVector = dir.normalized;
+				}
+				break;
+			case AiState.Retreating:
+				{
+					Vector3 dir3 = player.position - transform.position;
+					Vector2 dir = new(dir3.x, dir3.z);
+
+					characterMove.moveVector = -dir.normalized;
+				}
+				break;
+			default:
+				break;
 		}
 	}
 
-	void SetFlags()
+	void GetData()
 	{
-		flags = AiFlags.None;
-
 		// Raycast to player to see if we can see them
 		Vector3 dir = player.position - transform.position;
-		flags |= Physics.Raycast(transform.position, dir, dir.magnitude, visibilityMask, QueryTriggerInteraction.Ignore) ? AiFlags.None : AiFlags.AwareOfPlayer;
+		canSeePlayer = !Physics.Raycast(transform.position, dir, dir.magnitude, visibilityMask, QueryTriggerInteraction.Ignore);
+	}
+
+	void GetState()
+	{
+		if (canSeePlayer)
+		{
+			if (flags.HasFlag(AiFlags.Coward))
+			{
+				state = AiState.Retreating;
+				return;
+			}
+			else if (flags.HasFlag(AiFlags.Brave))
+			{
+				state = AiState.Attacking;
+				return;
+			}
+
+			if (health < 50)
+			{
+				state = AiState.Retreating;
+				return;
+			}
+
+			state = AiState.Attacking;
+			return;
+		}
+
+		state = AiState.Wandering;
 	}
 }
